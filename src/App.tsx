37c2ff594279994c7438781843d034f1e511e106
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-07-08 07:16:23 +0100

    scroll again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { PlusCircleIcon } from '@heroicons/react/20/solid';
 import { useInView } from 'react-intersection-observer';
 import Jumbo from './components/Jumbo';
-import HeavenScroll from 'react-heaven-scroll';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
 import Section4 from './components/Section4';
